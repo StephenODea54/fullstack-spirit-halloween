3f5718d8ae5002c3d0e4cc91210a1d78001f7c48
Stephen.Odea <Stephen.Odea@medmutual.com>
2023-11-08 20:52:25 -0500

    Add function to extract html from api response object

--- data/index.py
+++ data/index.py
@@ -1,11 +1,14 @@
 # Modules
+from bs4 import BeautifulSoup
 from typing import List
 import pandas as pd
 import os
+import requests
 
 
 # Constants
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_URL = 'https://maps.spirithalloween.com/api/getAsyncLocations?template=domain&level=domain&search='
 
 
 # https://data.opendatasoft.com/explore/dataset/georef-united-states-of-america-zc-point%40public/export/?flg=en-us
@@ -16,3 +19,14 @@ def read_zip_codes(file_path: str) -> List[int]:
     )
 
     return df['Zip Code'].tolist()
+
+def get_html(zip_code: int) -> List[str]:
+    request = requests.get(BASE_URL + str(zip_code))
+    markers = request.json()['markers']
+
+    html_arr: List[str] = []
+
+    for marker in markers:
+        html_arr.append(marker['info'])
+    
+    return html_arr
