aa66af24f9664e3a0c8bf038116b11140918cef8
Stephen.Odea <Stephen.Odea@medmutual.com>
2023-11-08 20:52:45 -0500

    Create builder class to parse html results

--- data/index.py
+++ data/index.py
@@ -1,5 +1,6 @@
 # Modules
 from bs4 import BeautifulSoup
+from dataclasses import asdict, dataclass, field
 from typing import List
 import pandas as pd
 import os
@@ -11,6 +12,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_URL = 'https://maps.spirithalloween.com/api/getAsyncLocations?template=domain&level=domain&search='
 
 
+# Helper Functions
 # https://data.opendatasoft.com/explore/dataset/georef-united-states-of-america-zc-point%40public/export/?flg=en-us
 def read_zip_codes(file_path: str) -> List[int]:
     df = (pd
@@ -30,3 +32,45 @@ def get_html(zip_code: int) -> List[str]:
         html_arr.append(marker['info'])
     
     return html_arr
+
+
+# Data that represents row in the DB
+@dataclass
+class Data:
+    address: str = field(init = False)
+    city: str = field(init = False)
+    state: str = field(init = False)
+    zip: int = field(init = False)
+    former_business: str = field(init = False)
+
+
+# Builder Class to contruct the rows for the data
+class DataframeBuilder:
+    def __init__(self, html: str) -> None:
+        self.data = Data()
+        self.soup = BeautifulSoup(html, 'html.parser')
+
+    def get_address_attributes(self) -> None:
+        address_div = self.soup.find('div', {'class': 'address'})
+        address_div_children = address_div.findChildren('div', recursive = False)
+
+        address_line_one = address_div_children[1].text
+        address_line_two = address_div_children[2].text
+
+        self.data.address = address_line_one
+        self.data.city = address_line_two.split(' ')[0][:-1]
+        self.data.state = address_line_two.split(' ')[1]
+        self.data.zip = int(address_line_two.split(' ')[2])
+
+    def get_former_business(self) -> None:
+        former_business_div = self.soup.find('div', {'class': 'address'})
+        former_business_clean = former_business_div.replace('Former ', '')
+
+        self.data.former_business = former_business_clean
+        return self
+    
+    def build(self) -> pd.DataFrame:
+        # Little ugly
+        self.get_address_attributes().get_former_business()
+
+        return pd.DataFrame([asdict(self.data)])
