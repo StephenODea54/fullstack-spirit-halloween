8f919adc573daa1b328e5828299d2f50a9a1d039
Stephen.Odea <Stephen.Odea@medmutual.com>
2023-11-11 21:02:06 -0500

    Add get former business counts route and service

--- api/src/routes/businesses.ts
+++ api/src/routes/businesses.ts
@@ -4,7 +4,20 @@ import express from 'express';
 // Services Import
 import businessServices from '@/services/businessServices.js';
 
+// Types
+import type { FormerBusiness, TypedRequestQuery } from '@/types/index.js';
+
 const router = express.Router();
+
+router.get(
+    '/counts',
+    (req: TypedRequestQuery<FormerBusiness[], { sort: 'ASC' | 'DESC'; limit: number }>, res) => {
+        const { sort, limit } = req.query;
+        const stateCounts = businessServices.getFormerBusinessCounts(sort, limit);
+
+        res.send(stateCounts);
+    },
+);
 
 router.get('/max', (_req, res) => {
     const highestFormerBusinessCount = businessServices.getHighestFormerBusiness();
--- api/src/services/businessServices.ts
+++ api/src/services/businessServices.ts
@@ -1,9 +1,36 @@
 // Module Imports
-import { desc, eq, sql } from 'drizzle-orm';
+import { asc, desc, eq, sql } from 'drizzle-orm';
 
 // DB Imports
 import { db } from '@/db/db.js';
 import * as schema from '@/db/schema.js';
+
+// Types
+import type { FormerBusiness } from '@/types/index.js';
+
+const getFormerBusinessCounts = (sort: 'ASC' | 'DESC' = 'DESC', limit: number = 10): FormerBusiness[] => {
+    const results = db
+        .select({
+            id: schema.formerBusinesses.id,
+            formerBusiness: schema.formerBusinesses.formerBusiness,
+            totalLocations: sql<number>`COUNT(locations.former_business_id)`,
+        })
+        .from(schema.locations)
+        .leftJoin(
+            schema.formerBusinesses,
+            eq(schema.locations.formerBusinessId, schema.formerBusinesses.internalId),
+        )
+        .groupBy(schema.formerBusinesses.id, schema.formerBusinesses.formerBusiness)
+        .orderBy(
+            sort === 'DESC'
+                ? desc(sql<number>`COUNT(locations.former_business_id)`)
+                : asc(sql<number>`COUNT(locations.former_business_id)`),
+        )
+        .limit(limit)
+        .all();
+
+    return results;
+};
 
 const getHighestFormerBusiness = (): { formerBusiness: string; totalLocations: number } => {
     const result = db
@@ -25,5 +52,6 @@ const getHighestFormerBusiness = (): { formerBusiness: string; totalLocations: n
 };
 
 export default {
+    getFormerBusinessCounts,
     getHighestFormerBusiness,
 };
