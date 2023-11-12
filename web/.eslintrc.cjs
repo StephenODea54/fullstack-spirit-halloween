3277c8d508c8c73239088e5e72b842d8109ccc06
Stephen.Odea <Stephen.Odea@medmutual.com>
2023-11-12 09:15:35 -0500

    Scaffold Front End

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:import/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'eslint-config-prettier',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+};
---
+++ web/.prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    printWidth: 110,
+};
