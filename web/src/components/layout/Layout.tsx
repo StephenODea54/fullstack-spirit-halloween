41296ca5c2fcdd1d2322029dcb66bfa416930d09
Stephen.Odea <Stephen.Odea@medmutual.com>
2023-11-14 14:55:41 -0500

    Change dashboard name

--- web/src/components/layout/Layout.tsx
+++ web/src/components/layout/Layout.tsx
@@ -1,20 +1,20 @@
 // Components
-import {Text, Title} from '@tremor/react';
+import { Text, Title } from '@tremor/react';
 
 // Types
 interface LayoutProps {
     children: React.ReactNode;
 }
 
-export const Layout = ({children}: LayoutProps) => {
+export const Layout = ({ children }: LayoutProps) => {
     return (
         <div className='min-h-screen bg-slate-50 dark:bg-slate-950'>
             <main className='container mx-auto px-4 py-12 sm:px-6 lg:px-8'>
                 <div className='flex flex-col space-y-6'>
                     <div>
-                        <Title>Dashboard</Title>
+                        <Title>Spirit Halloween - Economic Analysis</Title>
                         <Text>
-                            An alaysis of Spirit Halloween locations and the former businesses they occupied.
+                            Inspired by u/plutoandmal
                         </Text>
                     </div>
                     {children}
