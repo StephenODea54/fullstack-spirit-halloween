2d918f3bb6659b30ccbdbcddc96d58df013d8dd8
Stephen.Odea <Stephen.Odea@medmutual.com>
2023-11-15 14:59:41 -0500

    Format python code with black

--- db/extract/extract.py
+++ db/extract/extract.py
@@ -6,35 +6,32 @@ import pandas as pd
 import os
 import requests
 
-
 # Constants
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-BASE_URL = 'https://maps.spirithalloween.com/api/getAsyncLocations?template=domain&level=domain&search='
+BASE_URL = "https://maps.spirithalloween.com/api/getAsyncLocations?template=domain&level=domain&search="
 
 
 # Helper Functions
 # https://data.opendatasoft.com/explore/dataset/georef-united-states-of-america-zc-point%40public/export/?flg=en-us
 def read_zip_codes(file_path: str) -> List[int]:
-    df = (pd
-        .read_csv(file_path)
-        .filter(items = ['Zip Code'])
-    )
+    df = pd.read_csv(file_path).filter(items=["Zip Code"])
 
-    return df['Zip Code'].tolist()
+    return df["Zip Code"].tolist()
+
 
 def get_html(zip_code: int) -> Union[List[str], None]:
     request = requests.get(BASE_URL + str(zip_code))
 
     # Not all zip codes are guaranteed to return any results from the api call
-    markers = request.json()['markers']
+    markers = request.json()["markers"]
     if markers is None:
         return
 
     html_arr: List[str] = []
 
     for marker in markers:
-        html_arr.append(marker['info'])
-    
+        html_arr.append(marker["info"])
+
     return html_arr
 
 
@@ -52,20 +49,20 @@ class Data:
 class DataframeBuilder:
     def __init__(self, html: str) -> None:
         self.data = Data()
-        self.soup = BeautifulSoup(html, 'html.parser')
+        self.soup = BeautifulSoup(html, "html.parser")
 
     def get_address_attributes(self) -> None:
-        address_div = self.soup.find('div', {'class': 'address'})
-        address_div_children = address_div.findChildren('div', recursive = False)
+        address_div = self.soup.find("div", {"class": "address"})
+        address_div_children = address_div.findChildren("div", recursive=False)
 
         # Represents the street address e.g. 1234 Something Ave
         street_address = address_div_children[1].text
 
         # Represents the City
-        city = address_div_children[2].text.split(', ')[0]
+        city = address_div_children[2].text.split(", ")[0]
 
         # Represents the State and Zip part of the address
-        state_zip_arr = address_div_children[2].text.split(', ')[-1].split(' ')
+        state_zip_arr = address_div_children[2].text.split(", ")[-1].split(" ")
 
         # Excluding Canadian address from analysis b/c I'm lazy
         if not len(state_zip_arr) > 2:
@@ -77,21 +74,22 @@ class DataframeBuilder:
         return self
 
     def get_former_business(self) -> None:
-        former_business_div = self.soup.find('div', {'class': 'address-two'})
-        former_business_clean = former_business_div.text.replace('Former ', '')
+        former_business_div = self.soup.find("div", {"class": "address-two"})
+        former_business_clean = former_business_div.text.replace("Former ", "")
 
         self.data.former_business = former_business_clean
         return self
-    
+
     def build(self) -> pd.DataFrame:
         self.get_address_attributes().get_former_business()
 
         return pd.DataFrame([asdict(self.data)])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     dfs: List[pd.DataFrame] = []
 
-    zip_codes = read_zip_codes(os.path.join(SCRIPT_DIR, '..', 'input/georef.csv'))
+    zip_codes = read_zip_codes(os.path.join(SCRIPT_DIR, "..", "input/georef.csv"))
 
     for zip_code in zip_codes:
         html_arr = get_html(zip_code)
@@ -99,12 +97,14 @@ if __name__ == '__main__':
         if not html_arr is None:
             for html in html_arr:
                 dataframe_builder = DataframeBuilder(html)
-                
+
                 df = dataframe_builder.build()
                 dfs.append(df)
 
-    (pd.concat(dfs)
-       .dropna()
-       .drop_duplicates()
-       .reset_index(drop = True)
-       .to_csv(os.path.join(SCRIPT_DIR, '..', 'output/data.csv'), index = False))
+    (
+        pd.concat(dfs)
+        .dropna()
+        .drop_duplicates()
+        .reset_index(drop=True)
+        .to_csv(os.path.join(SCRIPT_DIR, "..", "output/data.csv"), index=False)
+    )
--- db/load/load.py
+++ db/load/load.py
@@ -1,5 +1,14 @@
 # Module Imports
-from sqlalchemy import Column, create_engine, Engine, ForeignKey, Integer, MetaData, Table, Text
+from sqlalchemy import (
+    Column,
+    create_engine,
+    Engine,
+    ForeignKey,
+    Integer,
+    MetaData,
+    Table,
+    Text,
+)
 import os
 import pandas as pd
 
@@ -12,64 +21,77 @@ def create_tables(engine: Engine):
     metadata = MetaData()
 
     cities = Table(
-        'cities', metadata,
-        Column('internal_id', Integer, primary_key = True),
-        Column('id', Text, unique = True, nullable = False),
-        Column('city', Text, unique = True, nullable = False)
+        "cities",
+        metadata,
+        Column("internal_id", Integer, primary_key=True),
+        Column("id", Text, unique=True, nullable=False),
+        Column("city", Text, unique=True, nullable=False),
     )
 
     states = Table(
-        'states', metadata,
-        Column('internal_id', Integer, primary_key = True),
-        Column('id', Text, unique = True, nullable = False),
-        Column('state', Text, unique = True, nullable = False)
+        "states",
+        metadata,
+        Column("internal_id", Integer, primary_key=True),
+        Column("id", Text, unique=True, nullable=False),
+        Column("state", Text, unique=True, nullable=False),
     )
 
     zip_codes = Table(
-        'zip_codes', metadata,
-        Column('internal_id', Integer, primary_key = True),
-        Column('id', Text, unique = True, nullable = False),
-        Column('zip', Integer, unique = True, nullable = False)
+        "zip_codes",
+        metadata,
+        Column("internal_id", Integer, primary_key=True),
+        Column("id", Text, unique=True, nullable=False),
+        Column("zip", Integer, unique=True, nullable=False),
     )
 
     former_businesses = Table(
-        'former_businesses', metadata,
-        Column('internal_id', Integer, primary_key = True),
-        Column('id', Text, unique = True, nullable = False),
-        Column('former_business', Text, unique = True, nullable = False)
+        "former_businesses",
+        metadata,
+        Column("internal_id", Integer, primary_key=True),
+        Column("id", Text, unique=True, nullable=False),
+        Column("former_business", Text, unique=True, nullable=False),
     )
 
     locations = Table(
-        'locations', metadata,
-        Column('internal_id', Integer, primary_key = True),
-        Column('id', Text, unique = True, nullable = False),
-        Column('address', Text, unique = True, nullable = False),
-        Column('city_id', Integer, ForeignKey('cities.internal_id'), nullable = False),
-        Column('state_id', Integer, ForeignKey('cities.internal_id'), nullable = False),
-        Column('zip_id', Integer, ForeignKey('cities.internal_id'), nullable = False),
-        Column('former_business_id', Integer, ForeignKey('cities.internal_id'), nullable = False)
+        "locations",
+        metadata,
+        Column("internal_id", Integer, primary_key=True),
+        Column("id", Text, unique=True, nullable=False),
+        Column("address", Text, unique=True, nullable=False),
+        Column("city_id", Integer, ForeignKey("cities.internal_id"), nullable=False),
+        Column("state_id", Integer, ForeignKey("cities.internal_id"), nullable=False),
+        Column("zip_id", Integer, ForeignKey("cities.internal_id"), nullable=False),
+        Column(
+            "former_business_id",
+            Integer,
+            ForeignKey("cities.internal_id"),
+            nullable=False,
+        ),
     )
 
     metadata.create_all(engine)
 
+
 def read_output_data(csv_name: str) -> pd.DataFrame:
-    return pd.read_csv(os.path.join(SCRIPT_DIR, '..', f'output/{csv_name}.csv'))
+    return pd.read_csv(os.path.join(SCRIPT_DIR, "..", f"output/{csv_name}.csv"))
+
 
 def insert_data(df: pd.DataFrame, table_name: str, engine: Engine) -> None:
-    df.to_sql(table_name, engine, if_exists = 'replace', index = False)
+    df.to_sql(table_name, engine, if_exists="replace", index=False)
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///api/database.db')
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///api/database.db")
     create_tables(engine)
 
-    cities_df = read_output_data('cities')
-    businesses_df = read_output_data('former_businesses')
-    locations_df = read_output_data('locations')
-    states_df = read_output_data('states')
-    zip_codes_df = read_output_data('zip_codes')
+    cities_df = read_output_data("cities")
+    businesses_df = read_output_data("former_businesses")
+    locations_df = read_output_data("locations")
+    states_df = read_output_data("states")
+    zip_codes_df = read_output_data("zip_codes")
 
-    insert_data(cities_df, 'cities', engine)
-    insert_data(businesses_df, 'former_businesses', engine)
-    insert_data(locations_df, 'locations', engine)
-    insert_data(states_df, 'states', engine)
-    insert_data(zip_codes_df, 'zip_codes', engine)
+    insert_data(cities_df, "cities", engine)
+    insert_data(businesses_df, "former_businesses", engine)
+    insert_data(locations_df, "locations", engine)
+    insert_data(states_df, "states", engine)
+    insert_data(zip_codes_df, "zip_codes", engine)
